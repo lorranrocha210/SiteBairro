a4f540c551d7a27426b6139a323678bb88e26b06
lorranrocha210 <117235981+lorranrocha210@users.noreply.github.com>
2022-11-06 23:39:35 -0300

    Add files via upload

---
+++ Site/script.js
@@ -0,0 +1,12 @@
+function mudar_pag_Init(){
+    window.location.href='index.html'
+}
+function mudar_pag_Transp(){
+    window.location.href='Transporte.html'
+}
+function mudar_pag_Educ(){
+    window.location.href='Educação.html'
+}
+function mudar_pag_Mor(){
+    window.location.href='Moradia.html'
+}
